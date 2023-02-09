f1780060b8c9cbc3c5213bac4a45c57f22710b0e
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-10 00:59:45 +0500

    Find the heaviest dumbbell

---
+++ 006_Вumbbells/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 1;
+int b = 8;
+int c = 3;
+int d = 6;
+int e = 2;
+int max = a;
+if(max < b)
+{
+    max = b;
+}
+if(max < c)
+{
+    max = c;
+}
+if(max < d)
+{
+    max = d;
+}
+if(max < e)
+{
+    max = e;
+}
+Console.Write("max = ");
+Console.Write(max);
