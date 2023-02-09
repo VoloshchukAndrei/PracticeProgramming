ed75cae22615d78bd115c02e3ddc333222a59d08
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-10 00:30:04 +0500

    Best Danila

---
+++ 005_Best/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username == "Данила")
+{
+    Console.WriteLine("Ура!!! Это же ДАНЯ!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
