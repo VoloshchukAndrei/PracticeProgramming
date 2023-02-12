94b1138ad417ba8717bf8b29fe97ef0ab2a23847
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-13 01:09:08 +0500

    fillingMas

---
+++ 010_MasRandom/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] Col)
+{
+    int count = Col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(Col[position]);
+        position++;
+    }
+}
+
+int [] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
