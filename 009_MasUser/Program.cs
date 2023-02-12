6e3cffe7d3abfc2984bb9da21efbd304d8fbd869
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-13 00:47:09 +0500

    Mas1

---
+++ 009_MasUser/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] masUser = {15,77,13,24,56,16,77,8};
+
+int n = masUser.Length;
+int find = 77;
+
+int index = 0;
+
+while (index < n)
+{
+    if(masUser[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
