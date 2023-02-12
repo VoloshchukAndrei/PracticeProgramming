ad94dfd4945b141a38db9bf765d540a0c57e92b7
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-13 01:29:46 +0500

    fillingMas + index

--- 010_MasRandom/Program.cs
+++ 010_MasRandom/Program.cs
@@ -20,7 +20,29 @@ void PrintArray(int[] Col)
     }
 }
 
+int indexOff(int[] collection, int fihd)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == fihd)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int [] array = new int [10];
 
 FillArray(array);
 PrintArray(array);
+
+Console.WriteLine();
+
+int pos = indexOff(array, 44);
+Console.WriteLine(pos);
