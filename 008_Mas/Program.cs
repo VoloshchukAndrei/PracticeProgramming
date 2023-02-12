a515bf5f322ffb4bef8f22fc15a95a7323c1eccc
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-12 22:50:14 +0500

    no mas

---
+++ 008_Mas/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 1;
+int b = 8;
+int c = 3;
+int d = 6;
+int e = 2;
+int f = 13;
+int g = 17;
+int h = 4;
+int i = 9;
+
+int max = a;
+if(max < b) max = b;
+if(max < c) max = c;
+if(max < d) max = d;
+if(max < e) max = e;
+if(max < f) max = f;
+if(max < g) max = g;
+if(max < h) max = h;
+if(max < i) max = i;
+
+Console.Write("max = ");
+Console.Write(max);
